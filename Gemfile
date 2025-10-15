01fe3feefc9359a42d2f8b08e4b355cbab758124
Taiki Abe <hiyu@MacBook-Air.local>
2025-10-16 00:02:03 +0900

    Lint修正

--- Gemfile
+++ Gemfile
@@ -64,4 +64,4 @@ group :test do
   gem "selenium-webdriver"
 end
 
-gem 'rails-i18n', '~> 8.0'
+gem "rails-i18n", "~> 8.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "quote_pages#top"
 
-  resources :users, only: [:new, :create]
-  resource :account, only: [:show, :edit, :update, :destroy]
+  resources :users, only: [ :new, :create ]
+  resource :account, only: [ :show, :edit, :update, :destroy ]
 
   get    "login",  to: "user_sessions#new"
   post   "login",  to: "user_sessions#create"
