9661ee7f6dccbe65c5e5fbf037e48960f5e7afff
Taiki Abe <hiyu@MacBook-Air.local>
2025-10-16 07:23:37 +0900

    パスワード変更機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root "quote_pages#top"
 
   resources :users, only: [ :new, :create ]
-  
+
   resource :account, only: [ :show, :destroy ] do
-    get 'edit_password', to: 'accounts#edit_password'
-    patch 'update_password', to: 'accounts#update_password'
+    get "edit_password", to: "accounts#edit_password"
+    patch "update_password", to: "accounts#update_password"
   end
 
   get    "login",  to: "user_sessions#new"
